2ca006f7fb8e2509b1e76a10b8cc1a966b399741
Sudharsan M <sudhar4702@gmail.com>
2025-09-24 19:14:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "https://imposter-game-production-d3f7.up.railway.app", // frontend URL
+      "https://imposter-game-sudhar-45.onrender.com", // frontend URL
       "http://localhost:5173" // optional, for local dev
     ],
     methods: ["GET", "POST"]
